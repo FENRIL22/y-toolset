6ae2252aac0b4d2336d6a1d9ef8faf9f5eae403e
Yabunaka <yabunaka@pc06-65.is.kochi-u.ac.jp>
2017-09-05 18:29:49 +0900

    Update

---
+++ run.sh
@@ -0,0 +1,23 @@
+POS=${PWD}
+
+echo ${POS}
+
+#docker run -it --rm \
+#	-v ${POS}:/apps \
+#	-p 8080:8080 \
+#	-p 8000:8000 \
+#	-it \
+#	google/cloud-sdk \
+#	bash
+
+docker run -it --rm \
+	-v ${POS}:/apps \
+	-p 8080:8080 \
+	-p 8000:8000 \
+	gae:go \
+	bash -c "(cd apps; dev_appserver.py app.yaml)"
+
+	#-P \
+
+	#google/cloud-sdk \
+#az3r/cloud-sdk-golang-docker \
