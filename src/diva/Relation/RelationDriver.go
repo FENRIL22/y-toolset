79aa6e8218b9d1531672f515d9a4fb78e543f19c
fenril22 <p.rest.k1.ke.se@gmail.com>
2017-09-30 03:58:37 +0900

    Write Driver

--- src/diva/Relation/RelationDriver.go
+++ src/diva/Relation/RelationDriver.go
@@ -4,17 +4,16 @@ import (
 	//"fmt"
 	//"time"
 	//"net/http"
-	//"errors"
+	"errors"
 	"context"
 	//"google.golang.org/appengine"
-	//"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/datastore"
 	//"strconv"
 )
 
 /* == Struct ======================================================== */
 type RelationDriver struct{
 	context context.Context
-	databus chan []Relation
 }
 
 func NewRelationDriver(ctx context.Context) *RelationDriver {
@@ -26,23 +25,51 @@ func NewRelationDriver(ctx context.Context) *RelationDriver {
 }
 
 func (s *RelationDriver) init(ctx context.Context) {
-	s.databus = make(chan []Relation)
 	s.context = ctx
 }
 
 func (s *RelationDriver) deinit() {
-	//deinit
-	//sync datastore
-	//delete datastore connection
 }
 
-func (s *RelationDriver) Put(re []Relation) error {
+func (s *RelationDriver) Put(re Relation) error {
+	_ = datastore.NewKey(s.context, "Relation", "", 0, nil)
 	return nil
 }
 
-func (s *RelationDriver) Update(re []Relation) error {
+func (s *RelationDriver) PutMulti(re []Relation) error {
+	//key := datastore.NewKey(s.context, "Relation", "", 0, nil)
+	_ = datastore.NewKey(s.context, "Relation", "", 0, nil)
+
+	return nil
 }
 
-func (s *RelationDriver) Get(list []int64) ([]Relation, error) {
+func (s *RelationDriver) Get(id int64) (Relation, error) {
+	ctx := s.context
+	var rl []Relation
+
+	q := datastore.NewQuery("Relation").Filter("id =", id)
+	q.GetAll(ctx, &rl)
+
+	if len(rl) < 1 {
+		return nil, errors.New("Data Not Found")
+	} else if len(rl) > 1 {
+		return nil, errors.New("Unknown Error")
+	}
+
+	return rl[0], nil
+}
+
+func (s *RelationDriver) GetMulti(ids []int64) ([]Relation, error) {
 	return []Relation{}, nil
 }
+
+func (s *RelationDriver) GetAll() ([]Relation, error) {
+
+
+
+	return []Relation{}, nil
+}
+
+func (s *RelationDriver) Remove(){}
+func (s *RelationDriver) RemoveMulti(){}
+
