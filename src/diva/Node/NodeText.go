48f9c11cdace50d2e5cc1196aecdf4393a42665d
Yabunaka <yabunaka@pc06-65.is.kochi-u.ac.jp>
2017-11-03 19:59:10 +0900

    Update

--- src/diva/Node/NodeText.go
+++ src/diva/Node/NodeText.go
@@ -1,28 +1,34 @@
 package node
 
 import (
+	"fmt"
 )
 
 
 /* == Struct ======================================================== */
 type NodeText struct {
-	*NodeBase
+	//*NodeBase
+	NodeBase
 }
 
 func NewNodeText() *NodeText{
 	s := new(NodeText)
-	s.NodeBase = new(NodeBase)
+	//s.NodeBase = new(NodeBase)
 
 	s.init()
 
 	return s
 }
 
+
 func (s *NodeText) init() {
 	s.Title = "Text node desu"
 	s.DataType = "NodeText"
 }
 
+func (s *NodeText) SetText(str string) {
+}
+
 func (s *NodeText) String() string {
-	return "implementing Now... : NodeURL"
+	return fmt.Sprintln(s.NodeBase)
 }
