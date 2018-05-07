3cbb853cb658131417449059937373ed178a050b
fenril22 <p.rest.k1.ke.se@gmail.com>
2018-05-07 20:59:47 +0900

    Update

--- src/tasker/test/DsDriver.go
+++ src/tasker/test/DsDriver.go
@@ -16,6 +16,18 @@ func NewDSDriver(c context.Context) *DS {
 	return s
 }
 
+func Save() {
+	//interfaceを引数に取り，errorを返す
+	//基本的にuuidは生成時にセットし，keyとして用いる
+	//存在しないかをチェックし(もしかしたらkey生成時に検知してくれるか？)，上書きオプションがあれば上書きとする
+	//https://hori-ryota.com/blog/golang-highly-versatile-method-with-arbitary-options/
+}
+
+func Load() {
+	//基本的にreflectで変数の生成まで行う形とする
+	//無理そうなら型情報をmapの形で保存させ，それを元に生成させる形とする
+}
+
 //TODO:
 /*
 構造体を渡せば書き込みが出来，Getする時点で構造体の配列として返せる形にする．
